7325a2796f1d90e7df429c6b1253087f089db39c
0hugoantonio83 <contatohugoantonio@gmail.com>
2026-06-25 13:23:40 -0300

    Atualizações

--- projeto-qos-ml/archive/agente_live.py
+++ projeto-qos-ml/archive/agente_live.py
@@ -7,9 +7,7 @@ import threading
 import warnings
 warnings.filterwarnings("ignore")
 
-# ==========================================
 # CONFIGURAÇÕES GERAIS
-# ==========================================
 PLACA_DE_REDE = "enp4s0"
 NOME_DO_HOST_ZABBIX = 'Node_YouTube' 
 IP_DO_ZABBIX = '127.0.0.1'
@@ -37,9 +35,11 @@ try:
     while True:
         time.sleep(1) 
         
+        # Cria uma cópia da janela de pacotes e limpa o buffer principal
         pacotes_atuais = pacotes_buffer.copy()
         pacotes_buffer.clear()
         
+        # CENÁRIO 1: Rede ativa (temos dados para a IA analisar)
         if len(pacotes_atuais) > 1:
             tempos = [p[0] for p in pacotes_atuais]
             tamanhos = [p[1] for p in pacotes_atuais]
@@ -57,19 +57,32 @@ try:
             # Julgamento da IA
             dados_rede = [[vazao, densidade, tempo_medio, jitter, tempo_ativo, tempo_ocioso]]
             previsao = modelo_rf.predict(dados_rede)[0]
+            
+            status_num = int(previsao)
             status_ia = "🚨 Degradação" if previsao == 1 else "✅ Limpo"
+            tamanho_medio = float(np.mean(tamanhos))
+
+        else:
+            vazao = 0.0
+            densidade = 0
+            jitter = 0.0
+            tempo_ocioso = 1.0 # 100% de ociosidade neste 1 segundo
             
-            # Envio para o Zabbix
-            metricas = [
-                ItemValue(NOME_DO_HOST_ZABBIX, 'qos.status', int(previsao)),
-                ItemValue(NOME_DO_HOST_ZABBIX, 'qos.vazao', float(vazao)),
-                ItemValue(NOME_DO_HOST_ZABBIX, 'qos.jitter', float(jitter)),
-                ItemValue(NOME_DO_HOST_ZABBIX, 'qos.idle', float(tempo_ocioso)),
-                ItemValue(NOME_DO_HOST_ZABBIX, 'qos.tamanho', float(np.mean(tamanhos)))
-            ]
-            zabbix_sender.send(metricas)
-            
-            print(f"[{time.strftime('%H:%M:%S')}] Pkts: {densidade} | Vazão: {vazao} B/s | Jitter: {jitter:.4f} | IA: {status_ia}")
+            status_num = 2 # Novo código de status para o Grafana alertar queda total!
+            status_ia = "❌ Rede Inativa (Zero Tráfego)"
+            tamanho_medio = 0.0
+
+        # Envio para o Zabbix
+        metricas = [
+            ItemValue(NOME_DO_HOST_ZABBIX, 'qos.status', status_num),
+            ItemValue(NOME_DO_HOST_ZABBIX, 'qos.vazao', float(vazao)),
+            ItemValue(NOME_DO_HOST_ZABBIX, 'qos.jitter', float(jitter)),
+            ItemValue(NOME_DO_HOST_ZABBIX, 'qos.idle', float(tempo_ocioso)),
+            ItemValue(NOME_DO_HOST_ZABBIX, 'qos.tamanho', tamanho_medio)
+        ]
+        zabbix_sender.send(metricas)
+        
+        print(f"[{time.strftime('%H:%M:%S')}] Pkts: {densidade} | Vazão: {vazao} B/s | Jitter: {jitter:.4f} | IA: {status_ia}")
 
 except KeyboardInterrupt:
     print("\nTransmissão ao vivo encerrada.")
